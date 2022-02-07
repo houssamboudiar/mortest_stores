166fe2368a81ef99bd31222dc8cd9cd442832639
houssamboudiar <houssam.boudiar0107@gmail.com>
2022-02-07 14:06:23 +0100

    Fixed users auth (password encryption) & Chakra UI & React Router & Redux Integration

--- mortest_frontend/src/components/App.js
+++ mortest_frontend/src/components/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from "react";
+import { Stack, Text } from "@chakra-ui/react";
+import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
 
 import { Provider } from "react-redux";
-import { store } from "../store";
+// import { store } from "../store";
 
 class App extends Component {
 
   render() {
     return (
-      <Provider store={store}>
           <Fragment>
             <div>
-              
+                <Stack spacing={3}>
+                  <Text fontSize='6xl'>(6xl) In love with React & Next</Text>
+                  <Text fontSize='5xl'>(5xl) In love with React & Next</Text>
+                  <Text fontSize='4xl'>(4xl) In love with React & Next</Text>
+                  <Text fontSize='3xl'>(3xl) In love with React & Next</Text>
+                  <Text fontSize='2xl'>(2xl) In love with React & Next</Text>
+                  <Text fontSize='xl'>(xl) In love with React & Next</Text>
+                  <Text fontSize='lg'>(lg) In love with React & Next</Text>
+                  <Text fontSize='md'>(md) In love with React & Next</Text>
+                  <Text fontSize='sm'>(sm) In love with React & Next</Text>
+                  <Text fontSize='xs'>(xs) In love with React & Next</Text>
+                </Stack>
             </div>
           </Fragment>
-      </Provider>
     );
   }
 }
 
 export default App;
-
-const container = document.getElementById("app");
-render(<App />, container);
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,5 @@
 # Register your models here.
+from django import forms
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from django.contrib import admin
@@ -6,4 +7,36 @@ from django.contrib import admin
 class MyUserAdmin(UserAdmin):
     model = CustomUser
 
-admin.site.register(CustomUser)
+class UserCreationForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username',)
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserAdmin(UserAdmin):
+    # The forms to add and change user instances
+    add_form = UserCreationForm
+    list_display = ("username",)
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'first_name', 'last_name')}),
+        )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'is_active')}
+            ),
+        )
+
+    filter_horizontal = ()
+
+admin.site.register(CustomUser, CustomUserAdmin)
